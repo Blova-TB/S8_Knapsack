46687fcb18e9d45685182777c15c1b7523ca2363
Thomas Blanché <thomas.baptiste.blanche@gmail.com>
2025-03-12 17:32:25 +0100

    Genetique_solver.mutation()

--- Genetique_solver.py
+++ Genetique_solver.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 
 class Genetique_solver(Solver):
+    seed : int
     sad : Sad
     nb_iter : int
     nb_pop : int
@@ -20,7 +21,9 @@ class Genetique_solver(Solver):
     population : list[list[bool]]
 
 
-    def __init__(self, sad: Sad, nb_iter, nb_pop, nb_crossing_points, mutation_rate):
+    def __init__(self, sad: Sad, nb_iter, nb_pop, nb_crossing_points, mutation_rate, seed):
+        self.seed = seed
+        random.seed(self.seed)
         super().__init__(sad)
         self.nb_iter = nb_iter
         self.nb_pop = nb_pop
@@ -29,7 +32,6 @@ class Genetique_solver(Solver):
         self.population = [[0]*self.sad.nbItem for i in range(self.nb_pop)]
         self.create_rand_population()
         
-        
         test = self.calc_fitness_pop()
         for i in range(10):
             print(test[i])
@@ -37,16 +39,23 @@ class Genetique_solver(Solver):
     def create_rand_population(self):
         for i in range(self.nb_pop):
             for j in range(self.sad.nbItem):
-                self.population[i][j] = random.randint(0,1)
+                self.population[i][j] = random.getrandbits(1)
 
     def reproduction(self):
-        print("TODO")
+        print("reproduction")
 
     def croisement(self):
-        print("TODO")
+        print("croisement")
 
     def mutation(self):
-        print("TODO")
+        print("mutation")
+        for i in range(self.population.__len__()):
+            for j in range(self.population[i].__len__()):
+                if(random.random()<self.mutation_rate):
+                    if(self.population[i][j] == 1):
+                        self.population[i][j] = 0
+                    else:
+                        self.population[i][j] = 1
 
     def calc_fitness(self,id):
         return sum((self.sad.listItems[i].profit if self.population[id][i] else 0) for i in range(self.sad.nbItem))
@@ -57,8 +66,15 @@ class Genetique_solver(Solver):
             temp.append(self.calc_fitness(i))
         return temp
 
+    def affPop(self):
+        for sol in self.population:
+            print(sol)
+        
 
 
 sad = parser.loadFromFile("Data/pi-12-100-1000-001.kna")
-test = Genetique_solver(sad,10,10,1,0.02)
+test = Genetique_solver(sad,10,10,1,0.1,0)
+test.affPop()
+test.mutation()
+test.affPop()
 
